3b211d7bee7149f13b912cc02945c32e23e1fe20
sequ3nce <prbar002@gmail.com>
2023-08-20 12:54:55 +0300

    Adds logging

--- src/main/java/com/barabanov/HibernateRunner.java
+++ src/main/java/com/barabanov/HibernateRunner.java
@@ -2,12 +2,16 @@ package com.barabanov;
 
 import com.barabanov.entity.User;
 import com.barabanov.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 
+@Slf4j
 public class HibernateRunner
 {
+
     public static void main(String[] args)
     {
         User user = User.builder()
@@ -15,34 +19,27 @@ public class HibernateRunner
                 .lastname("Pigeon")
                 .username("petonheg0389@gmail.com")
                 .build();
+        log.info("User entity is in transient state: {}", user);
 
         try(SessionFactory sessionFactory = HibernateUtil.buildSessionFactory())
         {
-            try(Session session1 = sessionFactory.openSession())
+            Session session1 = sessionFactory.openSession();
+            try(session1)
             {
-                session1.beginTransaction();
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is started: {}", transaction);
 
                 session1.saveOrUpdate(user);
+                log.trace("User is in the persistence state: {}, session: {}", user, session1);
 
                 session1.getTransaction().commit();
             }
-
-            try(Session session2 = sessionFactory.openSession())
-            {
-                session2.beginTransaction();
-
-                User vlad = session2.get(User.class, "petonheg0389@gmail.com");
-                session2.evict(vlad);
-
-                vlad.setFirstname("NE Vlad");
-
-                session2.refresh(vlad);
-
-                vlad.setLastname("aaaaaa");
-
-                //session2.merge(user);
-                session2.getTransaction().commit();
-            }
+            log.warn("User is in detached state: {}, session: {}", user, session1);
+        }
+        catch (Exception exception) // тут нет другой checked ex, так что просто Exception
+        {
+            log.error("Exception occurred", exception);
+            throw exception;
         }
     }
 }
